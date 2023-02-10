5c62d4c3d114cb58bf73295f55ee67fd4ad23e6e
g0niii <gonilovemor@gmail.com>
2023-02-09 23:32:03 -0300

    09/02ultimo

--- js/carrito.js
+++ js/carrito.js
@@ -25,20 +25,20 @@ function cargarProductosCarrito() {
             div.innerHTML = `
         <img class="carrito-producto-img" src="${producto.imagen}" alt="${producto.productName}">
     <div class="carrito-producto-titulo">
-        <small>Titulo</small>
-        <h3>${producto.productName}</h3>
+        <small>Nombre</small>
+        <h3 class="center">${producto.productName}</h3>
     </div>
     <div class="carrito-producto-cantidad">
         <small>Cantidad</small>
-        <p>${producto.cantidad}</p>
+        <p class="center">${producto.cantidad}</p>
     </div>
     <div class="carrito-producto-precio">
         <small>Precio</small>
-        <p>${producto.precio}</p>
+        <p class="center">${producto.precio}</p>
     </div>
     <div class="carrito-producto-subtotal">
         <small>Subtotal</small>
-        <p>${producto.precio * producto.cantidad}</p>
+        <p class="center">${producto.precio * producto.cantidad}</p>
     </div> 
         <button class="carrito-producto-eliminar" id="${producto.id}"><i class="bi bi-trash"></i></button>`
             contenedorCarritoProductos.append(div)
